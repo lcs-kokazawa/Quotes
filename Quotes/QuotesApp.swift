5f9b029c8c486da25745ed40d3934e73da9453fb
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-16 13:09:37 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Kiho Okazawa on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
